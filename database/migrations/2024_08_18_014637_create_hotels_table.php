2aac0ce2ee077b23cc79defd30ee1d83dbb5e58f
srengty <srengty@gmail.com>
2024-08-18 09:02:52 +0700

    chore: Add HotelsController, Hotels model, HotelsFactory, create_hotels_table migration, and HotelsSeeder

---
+++ database/migrations/2024_08_18_014637_create_hotels_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->string('hotel_name');
+            $table->text('description');
+            $table->string('address');
+            $table->binary('thumbs')->comment('Hotel thumbnail');
+            $table->string('image')->comment('Hotel image');
+            $table->foreignIdFor(\App\Models\City::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
